7616d6469947793a19422f8a5c17d50efc4e732d
Infinitea-3 <rummel.nathan@yahoo.com>
2023-08-10 00:51:25 -0400

    Add Round Counter, Add Computer Points

--- rock-paper-scissors/rock-paper-scissors.js
+++ rock-paper-scissors/rock-paper-scissors.js
@@ -13,68 +13,60 @@ function getComputerChoice(){
     return computerChoice
 }
 
-function playRound(playerSelection, computerSelection){
-    //Compares Player's and Computer's Decision, Then Outputs Message
-    if (playerSelection == computerSelection){
-        let message = "You Tie!"
-        return message
-    }
-    else if (playerSelection == "ROCK" && computerSelection == "PAPER"){
-        let message = "You Lose!"
-        return message
-    }
-    else if (playerSelection == "ROCK" && computerSelection == "SCISSORS"){
-        let message = "You Win!"
-        return message
-    }
-    else if (playerSelection == "PAPER" && computerSelection == "ROCK"){
-        let message = "You Win!"
-        return message
-    }
-    else if (playerSelection == "PAPER" && computerSelection == "SCISSORS"){
-        let message = "You Lose!"
-        return message
-    }
-    else if (playerSelection == "SCISSORS" && computerSelection == "ROCK"){
-        let message = "You Lose!"
-        return message
-    }
-    else if (playerSelection == "SCISSORS" && computerSelection == "PAPER"){
-        let message = "You Win!"
-        return message
-    }
-}
-
-function game(playerChoice,totalScore){
-
+function playRound(playerSelection, score){
     const results = document.querySelector('#results');
 
-    let computerChooses = getComputerChoice();
-    if((playRound(playerChoice,computerChooses))=="You Win!"){
+    totalScore = score
+
+    //Compares Player's & Computer's Decision, Then Outputs Message & Score
+    let computerSelection = getComputerChoice();
+    
+    if (playerSelection == computerSelection){
+        results.textContent = "You Tie!"
+        score = totalScore + 0;
+    }
+    else if (playerSelection == "ROCK" && computerSelection == "PAPER"){
+        results.textContent = "You Lose."
+        score = totalScore + 0;
+    }
+    else if (playerSelection == "ROCK" && computerSelection == "SCISSORS"){
         results.textContent = "You Win!"
         score = totalScore + 1;
     }
-    else if ((playRound(playerChoice,computerChooses))=="You Tie!"){
-        results.textContent = "You Tie!"
-        score = totalScore + 0.5;
+    else if (playerSelection == "PAPER" && computerSelection == "ROCK"){
+        results.textContent = "You Win!"
+        score = totalScore + 1;
     }
-    else{
+    else if (playerSelection == "PAPER" && computerSelection == "SCISSORS"){
         results.textContent = "You Lose."
+        score = totalScore + 0;
     }
-    //results.replaceChild(resultsDiv, resultsDiv);
-
+    else if (playerSelection == "SCISSORS" && computerSelection == "ROCK"){
+        results.textContent = "You Lose."
+        score = totalScore + 0;
+    }
+    else if (playerSelection == "SCISSORS" && computerSelection == "PAPER"){
+        results.textContent = "You Win!"
+        score = totalScore + 1;
+    }
     return score
 }
 
 //Main Function
 let score = 0
+let round = 0
 
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button)=>{
     button.addEventListener('click',()=>{
-        score = game(button.id,score);
-        const container = document.querySelector('#container');
-        container.textContent = "Total Points:".concat(" ",score);
+        score = playRound(button.id, score);
+        round = round + 1
+        const roundContainer = document.querySelector('#roundContainer');
+        roundContainer.textContent = "Round:".concat(round);
+        const playerContainer = document.querySelector('#playerContainer');
+        playerContainer.textContent = "Player Total Points:".concat(" ",score);
+        const computerContainer = document.querySelector('#computerContainer');
+        computerContainer.textContent = "Player Total Points:".concat(" ",round - score);
     });
 });
 
